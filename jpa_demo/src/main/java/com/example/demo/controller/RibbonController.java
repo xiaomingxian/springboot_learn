6190a364a56a6667d3e1c5870cf6d46194f2e0ca
xiaoming.xian <xiaoming.xian@cintel.com.cn>
2019-03-08 14:58:51 +0800

    Ribbon_Feign理解

--- jpa_demo/src/main/java/com/example/demo/controller/RibbonController.java
+++ jpa_demo/src/main/java/com/example/demo/controller/RibbonController.java
@@ -20,13 +20,12 @@ public class RibbonController {
     public Map useService() {
         Map body = new HashMap<>();
         String service_name = "SERVER";
-//        for (int x = 0; x < 10; x++) {
-
+        for (int x = 0; x < 10; x++) {
+            //ribbon是客户端的负载均衡，在发送请求前，获取到服务实例
             ResponseEntity<Map> forEntity = restTemplate.getForEntity("http://" + service_name + "/learn/springclould_test", Map.class);
             body = forEntity.getBody();
-            System.out.printf("-------->调用结果:" + body);
-//        }
-
+            System.out.println("-------->调用结果:" + body);
+        }
         return body;
     }
 
