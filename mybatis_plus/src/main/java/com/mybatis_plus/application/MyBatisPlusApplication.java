cbc58a2bd4541dbfdee50eb4ea3d21f2843ca1fd
xiaoming.xian <xiaoming.xian@cintel.com.cn>
2019-03-12 17:39:34 +0800

    mybatisPlus配置待解决

--- mybatis_plus/src/main/java/com/mybatis_plus/application/MyBatisPlusApplication.java
+++ mybatis_plus/src/main/java/com/mybatis_plus/application/MyBatisPlusApplication.java
@@ -4,13 +4,10 @@ package com.mybatis_plus.application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//既作为客户端又是服务端-配置多个实例来相互注册和自我注册-实现高可用
-@EnableDiscoveryClient//作为客户端
-@SpringBootApplication
 //mybatis扫描使用
-@MapperScan("xxm.dao")
+@MapperScan("com.mybatis_plus.dao")//或者：@Mapper
+@SpringBootApplication
 public class MyBatisPlusApplication {
 
     public static void main(String[] args) {
---
+++ mybatis_plus/src/test/java/mybatis_plus/T1_MyBatisPlusTest.java
@@ -0,0 +1,12 @@
+package mybatis_plus;
+
+import com.mybatis_plus.application.MyBatisPlusApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyBatisPlusApplication.class )
+public class T1_MyBatisPlusTest {
+
+}
