debb00065558b657712a41e62583313f30e261f4
xxm <934792834@qq.com>
2019-07-03 23:38:46 +0800

    starter中的页面与接口访问--临时解决方案

--- myself_stater/src/main/java/com/xxmst/controller/TestStController.java
+++ myself_stater/src/main/java/com/xxmst/controller/TestStController.java
@@ -2,18 +2,27 @@ package com.xxmst.controller;
 
 import com.xxmst.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("start")
-public class TestController {
+public class TestStController {
     @Autowired
     private HelloService helloService;
 
     @GetMapping("hello")
+    @ResponseBody
     public String hello() {
         return helloService.getMsg();
     }
+
+    @GetMapping("page")
+    public String getPage(){
+        return "/hello.html";
+    }
+
+
 }
