fd36885e5e62a47538c64f70cfdda9802eb3b84f
xxm <934792834@qq.com>
2019-11-02 23:08:56 +0800

    豪猪监控

---
+++ eureka_parent/eureka_server/src/main/java/xxm/application/Application.java
@@ -0,0 +1,47 @@
+package xxm.application;
+
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@EnableCircuitBreaker
+//既作为客户端又是服务端-配置多个实例来相互注册和自我注册-实现高可用
+@EnableEurekaServer//来开启服务注册中心
+@EnableDiscoveryClient//作为客户端??//服务发现
+@SpringBootApplication
+@ComponentScan(basePackages = {"xxm.controller", "xxm.service"})
+//mybatis扫描使用
+@MapperScan("xxm.dao")
+//activemq两种方式不可同时使用？？？？
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+        System.out.println("<-------------------启动类------------------->");
+
+    }
+
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
---
+++ eureka_parent/hystrix-dashboard/src/main/java/com/xxm/dashboard/App.java
@@ -0,0 +1,33 @@
+package com.xxm.dashboard;
+
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class App {
+    //http://localhost:9000/hystrix
+
+    //消费者加配置
+    //访问：  http://localhost:9999/hystrix.stream
+    //初次会 ping 访问服务降级/熔断节点后刷新
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
