c013d1f0fc97248074868cccf3a07b6184c47d5e
xxm-xiaoxin <934792834@qq.com>
2020-05-07 16:29:00 +0800

    redis 解决的问题

---
+++ x2_xingneng/src/test/java/redis/code/T1.java
@@ -0,0 +1,8 @@
+package redis.code;
+
+public class T1 {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        System.out.println(thread.getName());
+    }
+}
