7543211cbf96d5385156ce023e87188254db2cb7
xxm-xiaoxin <934792834@qq.com>
2020-07-16 17:13:55 +0800

    docker安装配置

---
+++ rabbitmq_learn/src/main/java/xxm/controller/TestMq.java
@@ -0,0 +1,64 @@
+package xxm.controller;
+
+import com.rabbitmq.client.*;
+import org.junit.Test;
+import xxm.utils.ConnectUtil;
+
+import java.io.IOException;
+
+public class TestMq {
+
+    private static final String QUEUE_NAME = "test_simple_queue";
+
+    @Test
+    public void sendMsg() {
+        try {
+
+            Connection connect = ConnectUtil.getConnect();
+            //从连接中获取一个通道
+            Channel channel = connect.createChannel();
+            //String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            //
+            String msg = "消息1";
+            channel.basicPublish("", QUEUE_NAME, null, msg.getBytes());
+
+            channel.close();
+            connect.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void receiveMsg() {
+
+
+        try {
+
+            Connection connect = ConnectUtil.getConnect();
+            Channel channel = connect.createChannel();
+
+            //创建消费者
+            DefaultConsumer consumer = new DefaultConsumer(channel) {
+
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                        throws IOException {
+                    String message = new String(body, "UTF-8");
+                    System.out.println("接收到一个消息： " + message);
+                }
+
+            };
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ rabbitmq_learn/src/main/java/xxm/utils/ConnectUtil.java
@@ -0,0 +1,28 @@
+package xxm.utils;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ConnectUtil {
+
+    public static Connection getConnect() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("49.234.25.12");
+        factory.setPort(5672);
+        factory.setVirtualHost("/my_vhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
