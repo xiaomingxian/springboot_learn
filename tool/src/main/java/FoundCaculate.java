0f2eb97e2993152406a00f126648ec7df01f33e3
xxm-xiaoxin <934792834@qq.com>
2021-01-25 16:42:22 +0800

    提交

---
+++ tool/src/main/java/FoundCaculate.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FoundCaculate
+
+{
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+        Map<String, Map<Integer, Double>> type = new HashMap<String, Map<Integer, Double>>();
+
+
+
+
+
+
+    }
+}
